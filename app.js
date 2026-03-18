86c7de30714f79c58a55622a43b35f2317662ee4
sinannotificacao <sinannotificacao@gmail.com>
2026-03-18 18:53:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('fillSampleBtn').addEventListener('click', () => alert('Preencher exemplo ainda não implementado.'));
-  document.getElementById('clearBtn').addEventListener('click', () => document.querySelectorAll('input, select, textarea').forEach(el => el.value = ''));
+  document.getElementById('clearBtn').addEventListener('click', () => {
+    document.querySelectorAll('input, select, textarea').forEach(el => el.value = '');
+  });
   document.getElementById('exportBtn').addEventListener('click', () => alert('Exportar PDF ainda não implementado.'));
 });
