ce6723900f3197277927da3e85605d8a1622eebf
sinannotificacao <sinannotificacao@gmail.com>
2026-03-30 15:42:39 -0300

    Add files via upload

--- app-surto.js
+++ app-surto.js
@@ -1,25 +1,25 @@
 async function exportPDF(){
-const {PDFDocument,StandardFonts, rgb, degrees}=PDFLib;
+const {PDFDocument,StandardFonts,degrees}=PDFLib;
 const pdfDoc=await PDFDocument.create();
 const page=pdfDoc.addPage([595,842]);
 const font=await pdfDoc.embedFont(StandardFonts.Helvetica);
 
 page.drawText("DADOS GERAIS",{x:20,y:700,rotate:degrees(90),size:10,font});
-page.drawText("NOTIFICAÇÃO DE SURTO",{x:20,y:500,rotate:degrees(90),size:10,font});
-page.drawText("DADOS DE OCORRÊNCIA",{x:20,y:300,rotate:degrees(90),size:10,font});
-page.drawText("SITUAÇÃO INICIAL",{x:20,y:150,rotate:degrees(90),size:10,font});
+page.drawText("NOTIFICAÇÃO DE SURTO",{x:20,y:520,rotate:degrees(90),size:10,font});
+page.drawText("DADOS DE OCORRÊNCIA",{x:20,y:340,rotate:degrees(90),size:10,font});
+page.drawText("SITUAÇÃO INICIAL",{x:20,y:180,rotate:degrees(90),size:10,font});
 
-const form=document.getElementById("form");
-page.drawText("Agravo: "+form.agravo.value,{x:100,y:750,size:10,font});
-page.drawText("Município: "+form.municipio.value,{x:100,y:730,size:10,font});
-page.drawText("Casos: "+form.casos.value,{x:100,y:710,size:10,font});
-page.drawText("Obs: "+form.obs.value,{x:100,y:680,size:10,font});
+const f=document.getElementById("form");
+page.drawText("Agravo: "+f.agravo.value,{x:120,y:760,size:10,font});
+page.drawText("Município: "+f.municipio.value,{x:120,y:740,size:10,font});
+page.drawText("Casos: "+f.casos.value,{x:120,y:720,size:10,font});
+page.drawText("Obs: "+f.obs.value,{x:120,y:690,size:10,font,maxWidth:400});
 
-const pdfBytes=await pdfDoc.save();
-const blob=new Blob([pdfBytes],{type:"application/pdf"});
+const bytes=await pdfDoc.save();
+const blob=new Blob([bytes],{type:"application/pdf"});
 const url=URL.createObjectURL(blob);
 const a=document.createElement("a");
 a.href=url;
-a.download="ficha-surto-v2.pdf";
+a.download="ficha-surto.pdf";
 a.click();
 }
